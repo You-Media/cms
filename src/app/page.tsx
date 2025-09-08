26f5f5af74fea452681bf3aeceda76deebde6f16
GiovTemp <giovannitempestainfo@gmail.com>
2025-09-09 00:15:21 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
-"use client"
-
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
 import { APP_ROUTES } from '@/config/routes'
 
 export default function HomePage() {
-  const router = useRouter()
-  useEffect(() => {
-    router.replace(APP_ROUTES.AUTH.LOGIN)
-  }, [router])
-  return null
+  redirect(APP_ROUTES.AUTH.LOGIN)
 }
